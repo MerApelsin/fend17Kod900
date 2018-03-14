833ab263c55d80c936e36882da1e8157a3b20602
stormsteffe <32032941+stormsteffe@users.noreply.github.com>
2018-03-14 09:52:39 +0100

    Sökfunktion funkar

--- js/sokresultat.js
+++ js/sokresultat.js
@@ -1,12 +1,32 @@
+async function myFunction(){
 
-function myFunction(){
-  let input, filter;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
+  let searchResultList = document.getElementById('searchResult');
 
-  
+  let searchValue = document.getElementById('search').value;
+  let filter = searchValue.toUpperCase();
+
+  let allNames = await fetchData("http://nackowskis.azurewebsites.net/api/Auktion/900");
+
+  let createPtag = document.createElement('p')
+
+  let leta = allNames.filter(name => name.Titel.toUpperCase().includes(filter));
+  console.log(JSON.stringify(leta));
+
+  let textNode = document.createTextNode(JSON.stringify(leta));
+
+
+  //console.log(textNode);
+  createPtag.appendChild(textNode);
+  searchResultList.appendChild(createPtag);
+
+
+
 }
 
-function click(){
-
+async function fetchData(url){
+    let promise = await fetch(url);
+    let data = await promise.json();
+    return data;
 }
+
+/*myFunction();*/
